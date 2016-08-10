71e210e4c441a43538171b200dc06bb18a1c09d6
Mark Nuiver <mark@vrijblikken.nl>
2016-08-10 15:14:30 +0200

    Add migrations users services

--- db/migrate/20160809151948_create_services.rb
+++ db/migrate/20160809151948_create_services.rb
@@ -1,14 +1,13 @@
 class CreateServices < ActiveRecord::Migration[5.0]
   def change
     create_table :services do |t|
-      t.string :name
-      t.text :description
-      t.datetime :date
-      t.string :address
-      t.string :zipcode
-      t.string :city
-      t.string :category
-      t.float  :price
+      t.string   :name
+      t.text     :description
+      t.datetime :begin_date
+      t.datetime :end_date
+      t.string   :location
+      t.string   :category
+      t.float    :price
 
       t.timestamps
     end
