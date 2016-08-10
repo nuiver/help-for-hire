c10b395fa1ec1dd8bd297b03c5793c2d20af6a11
Angela2389 <angela_2389@hotmail.com>
2016-08-10 15:14:18 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Service.create ( {name: "Monique", description: "I'm available for cleaning basically anything! I have helped out my family in needs for years and know the perfect ways how to clean best", date: "2016-08-27 00:00:00 +0200", address: "Plofkip 1", zipcode: "1625HR", city: "Hoorn", category: "cleaner", price: 100})
-Service.create ( {name: "Jacob", description: "I can do your gardening for you! I have my own company for 20 years. No matter how big or small your garden is, I can make it beautifull", date: "2016-09-20 00:00:00 +0200", address: "Weerhoutstraat 23", zipcode: "1600AX", city: "Amsterdam", category: "garderner", price: 200})
-Service.create ( {name: "Wouter", description: "Computer problems? I'm your guy to contact!", date: "2016-09-01 00:00:00 +0200", address: "Vaarwater 123", zipcode: "1600BB", city: "Alkmaar", catergory: "computerguy" price: 100})
+Service.create ( {name: "Monique", description: "I'm available for cleaning basically anything! I have helped out my family in needs for years and know the perfect ways how to clean best", begin_date: "2016-08-27 00:00:00 +0200", end_date: "2016-09-10 00:00:00 +0200" location: "Hoorn", category: "cleaner", price: 100})
+Service.create ( {name: "Jacob", description: "I can do your gardening for you! I have my own company for 20 years. No matter how big or small your garden is, I can make it beautifull", begin_date: "2016-09-20 00:00:00 +0200", end_date: "2016-09-28 00:00:00 +0200" location: "Amsterdam", category: "garderner", price: 200})
+Service.create ( {name: "Wouter", description: "Computer problems? I'm your guy to contact!", begin_date: "2016-09-01 00:00:00 +0200", end_date:"2016-09-02 00:00:00 +0200" location: "Alkmaar", catergory: "computerguy" price: 100})
 
 User.create ( {name: "Dana", email: "dana@gmail.com" username: "dana" password: "xaxaxa"} )
 User.create ( {name: "Jacob", email: "Jacob@gmail.com" username: "jacob" password: "babababa"})
