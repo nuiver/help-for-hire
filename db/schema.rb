818a0299f18694fdedc0c40b33e74c3325ff1015
Mark Nuiver <mark@vrijblikken.nl>
2016-08-11 16:00:30 +0200

    Relations user service and bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811134657) do
+ActiveRecord::Schema.define(version: 20160811135802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 20160811134657) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "service_id"
+    t.integer  "user_id"
     t.index ["service_id"], name: "index_bookings_on_service_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
   create_table "services", force: :cascade do |t|
@@ -35,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160811134657) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_services_on_user_id", using: :btree
   end
 
   create_table "services_tasks", id: false, force: :cascade do |t|
@@ -67,4 +71,6 @@ ActiveRecord::Schema.define(version: 20160811134657) do
   end
 
   add_foreign_key "bookings", "services"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "services", "users"
 end
