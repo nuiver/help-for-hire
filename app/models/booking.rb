2ae99a7eee57a035f2b41d84617d8ccf731de4e6
Angela2389 <angela_2389@hotmail.com>
2016-08-12 16:06:21 +0200

    availablecheck

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :service
   belongs_to :user
+
+  validate :is_available
+
+  def is_available
+    overlapping = Booking.where("begin_date BETWEEN ? AND ? OR end_date BETWEEN ? AND ?", begin_date, end_date, begin_date, end_date).count
+
+    return if overlapping == 0
+
+    errors.add :booking, "this room is not available"
+  end
 end
